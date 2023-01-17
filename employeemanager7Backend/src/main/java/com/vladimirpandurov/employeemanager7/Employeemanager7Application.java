8455db341864da8f97a74916260423324430f68e
panvla <pandvlado@gmail.com>
2023-01-17 12:05:43 +0100

    finish

---
+++ employeemanager7Backend/src/main/java/com/vladimirpandurov/employeemanager7/Employeemanager7Application.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employeemanager7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeemanager7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeemanager7Application.class, args);
+	}
+
+}
